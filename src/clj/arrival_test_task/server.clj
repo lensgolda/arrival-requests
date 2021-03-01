822b59f1e965c0bec060bca9c48e1575cc844865
lens4g <lensgolda@gmail.com>
2021-03-01 03:27:21 +0200

    * fix

--- src/clj/arrival_test_task/server.clj
+++ src/clj/arrival_test_task/server.clj
@@ -1,9 +1,23 @@
 (ns arrival-test-task.server
-  (:require [arrival-test-task.handler :refer [handler]]
+  (:require [com.stuartsierra.component :as component]
             [config.core :refer [env]]
             [ring.adapter.jetty :refer [run-jetty]])
-  (:gen-class))
+  (:import (org.eclipse.jetty.server Server)))
 
-(defn -main [& _args]
-  (let [port (or (env :port) 8080)]
-    (run-jetty handler {:port port :join? false})))
+(defrecord WebServer []
+  component/Lifecycle
+  (start [this]
+    (let [config  (get-in this [:config :jetty])
+          handler (get-in this [:handler :routes] false)
+          _       (assert config "config is missed or empty")
+          _       (assert handler "handler/routes key not found in system-map")]
+      (if (:jetty this)
+        this
+        (assoc this :jetty (run-jetty handler config)))))
+  (stop [this]
+    (when-let [jetty (:jetty this)]
+      (.stop ^Server jetty))
+    (assoc this :jetty nil)))
+
+(defn create []
+  (map->WebServer {}))
